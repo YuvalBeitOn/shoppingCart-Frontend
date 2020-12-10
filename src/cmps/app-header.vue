0fbb1f709696acfa3f51e7303bdc08eeab94ebb4
Yuval Beit On <yuvalbeiton@gmail.com>
2020-12-10 15:12:56 +0200

    ggg

--- src/cmps/app-header.vue
+++ src/cmps/app-header.vue
@@ -1,13 +1,28 @@
 <template>
-  <section class="app-header flex space-between align-center">
-    <router-link to="/"><h1>Miss Shop</h1></router-link>
-    <nav class="nav-menu">
-      <router-link to="/">Shop</router-link>
-      <router-link to="/cart"><i class="fas fa-shopping-cart"></i></router-link>
-    </nav>
+  <section class="app-header-container">
+    <section class="app-header flex space-between align-center">
+      <router-link to="/"><h1>Miss Shop</h1></router-link>
+      <nav class="nav-menu">
+        <router-link to="/">Shop</router-link>
+        <router-link to="/cart"
+          ><i class="fas fa-shopping-cart"></i
+        ></router-link>
+      </nav>
+    </section>
+    <img src="@/assets/img/shopping.png" alt="" />
+    <div>
+      <h2>Our Products</h2>
+      <span>My Cart</span>
+    </div>
   </section>
 </template>
 
 <script>
-export default {};
+export default {
+  computed: {
+    // cartItems() {
+    //   return this.$store.getters.cart.
+    // }
+  }
+};
 </script>
