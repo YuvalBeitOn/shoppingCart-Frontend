1164c2313fa6ec06b9d109414724da3998f2b8f9
Yuval Beit On <yuvalbeiton@gmail.com>
2020-12-10 14:14:25 +0200

    HHh

--- src/views/shop.vue
+++ src/views/shop.vue
@@ -3,8 +3,15 @@
 </template>
 
 <script>
+import productList from "../cmps/product-list";
 export default {
   components: {
+    productList,
+  },
+  computed: {
+    products() {
+      return this.$store.getters.products;
+    },
   },
 };
 </script>
